cf12560744b70ad33b9d7b77db95c98c30524780
Maicol Montoya <maicolmontoya323@gmail.com>
2025-10-07 22:26:38 -0500

    refactor(discovery-server): remove dotenv configuration

--- src/main/java/conectaseguros/co/discovery_server/DiscoveryServerApplication.java
+++ src/main/java/conectaseguros/co/discovery_server/DiscoveryServerApplication.java
@@ -1,7 +1,5 @@
 package conectaseguros.co.discovery_server;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import java.util.Objects;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -11,22 +9,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class DiscoveryServerApplication {
 
 	static void main(String[] args) {
-
-        Dotenv dotenv = Dotenv.configure()
-                .directory("./discovery-server")
-                .ignoreIfMissing()
-                .load();
-
-        System.setProperty("EUREKA_PORT", Objects.requireNonNull(dotenv.get("EUREKA_PORT")));
-
-        System.setProperty("EUREKA_PREFER_IP_ADDRESS", Objects.requireNonNull(dotenv.get("EUREKA_PREFER_IP_ADDRESS")));
-
-        System.setProperty("EUREKA_HOSTNAME_PEER1", Objects.requireNonNull(dotenv.get("EUREKA_HOSTNAME_PEER1")));
-        System.setProperty("EUREKA_HOSTNAME_PEER2", Objects.requireNonNull(dotenv.get("EUREKA_HOSTNAME_PEER2")));
-
-        System.setProperty("EUREKA_SELF_PRESERVATION", Objects.requireNonNull(dotenv.get("EUREKA_SELF_PRESERVATION")));
-        System.setProperty("EUREKA_RENEWAL_THRESHOLD", Objects.requireNonNull(dotenv.get("EUREKA_RENEWAL_THRESHOLD")));
-        System.setProperty("EUREKA_EVICTION_INTERVAL", Objects.requireNonNull(dotenv.get("EUREKA_EVICTION_INTERVAL")));
 
 		SpringApplication.run(DiscoveryServerApplication.class, args);
 	}
